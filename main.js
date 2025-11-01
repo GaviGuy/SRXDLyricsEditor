ce70e02edcefbd4f02ae2b4b06d2ad8c02a6ee49
GaviGuy <gaviguy711@gmail.com>
2025-11-01 17:42:11 -0600

    misc reformatting, lyrics preview

--- main.js
+++ main.js
@@ -26,6 +26,20 @@ function getWordWidth (word) {
     return len;
 }
 
+function appendToPreview(posX, posY, size, text) {
+    let parentElement = document.getElementById("lyrics-preview");
+    let newElement = parentElement.appendChild(document.createElement("p"));
+    newElement.style = `font-weight: bold; position: absolute; left: ${posX}rem; top: ${posY}rem; font-size:${size}rem`;
+    newElement.textContent = text;
+}
+
+function clearPreview() {
+    let parentElement = document.getElementById("lyrics-preview");
+    while(parentElement.hasChildNodes()) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+}
+
 function convertToRomaji(japaneseString) {
     //return "";
 }
@@ -55,7 +69,7 @@ function generateLyricString() {
     let initialPos = (availableSpace - kanaSpacing * japaneseString.length) / 2 + kanaSpacing / 2;
     let kanaSize = 1;
     let romajiSize = 0.6;
-    console.log(`initialPos ${initialPos}`);
+    // console.log(`initialPos ${initialPos}`);
 
     // console.log(romajiArray);
 
@@ -73,16 +87,17 @@ function generateLyricString() {
             word += romajiArray[i+j];
         }
         let wordWidth = getWordWidth(word) / 2.5;
-        console.log(`Width of ${word}: ${wordWidth}`);
-        console.log(`Length of ${word}: ${wordLength}`);
+        // console.log(`Width of ${word}: ${wordWidth}`);
+        // console.log(`Length of ${word}: ${wordLength}`);
         let centerPos = initialPos + kanaSpacing * (i + (wordLength / 2) - numSplit) - (kanaSpacing - kanaSize) / 2;
         let leftPos = centerPos - (wordWidth / 2)
-        console.log(`leftPos ${leftPos}`);
+        // console.log(`leftPos ${leftPos}`);
         let accumulatedOffset = 0;
 
         for(let j = 0; j < wordLength; j++) {
             let syllableWidth = getWordWidth(romajiArray[i+j]) / 2.5;
             // console.log(`Width ${syllableWidth}`);
+            appendToPreview(leftPos+accumulatedOffset, 0, romajiSize, romajiArray[i+j]);
 
             romajiPositions.push(leftPos + accumulatedOffset);
             accumulatedOffset += syllableWidth;
@@ -100,11 +115,20 @@ function generateLyricString() {
 
 
     //assemble final string
+    clearPreview();
     for(let i = 0; i < japaneseString.length; i++) {
         if(romajiArray[romajiIndex] == "|") romajiIndex++;
 
+        //truncate float numbers
         let kanaPos = initialPos + kanaSpacing * i;
+        kanaPos = Math.round(kanaPos * 100) / 100;
         let romajiPos = romajiPositions[i];
+        romajiPos = Math.round(romajiPos * 100) / 100;
+
+        appendToPreview(kanaPos, 0.5, kanaSize, japaneseString[i]);
+
+        appendToPreview(romajiPos, 0, romajiSize, romajiArray[romajiIndex]);
+
         returnString += `<pos=${kanaPos}em><size=${kanaSize}em>`
                 + japaneseString[i]
                 + `<pos=${romajiPos}em><voffset=1em><size=${romajiSize}em>`
